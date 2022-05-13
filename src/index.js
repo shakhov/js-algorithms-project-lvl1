c9d61313b26cd171e4196dcfb97fd062936780d6
shakhov <shahov.a.a@gmail.com>
2022-05-13 11:48:14 +0300

    Engine builds inverted index

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
-const buildSearchEngine = (docs = []) => {
-  const search = (word) => (
-    docs
-      .filter(({ text }) => text.split(' ').includes(word))
-      .map(({ id }) => id)
-  );
+const getTextWords = (text = '') => (
+  text.split(' ').filter((w) => w !== '')
+);
 
-  return { docs, search };
+const buildDocumentIndex = (({ id, text }, initialIndex = {}) => {
+  const documentWords = getTextWords(text);
+  return documentWords.reduce((acc, word) => {
+    const documentIds = acc[word] || new Set();
+    return { ...acc, [word]: documentIds.add(id) };
+  }, initialIndex);
+});
+
+const buildSearchEngine = (documents = []) => {
+  const invertedIndex = documents.reduce((indexAcc, document) => (
+    buildDocumentIndex(document, indexAcc)
+  ), {});
+
+  const search = (word) => [...(invertedIndex[word] || [])];
+  return {
+    documents,
+    search,
+  };
 };
 
 export default buildSearchEngine;
