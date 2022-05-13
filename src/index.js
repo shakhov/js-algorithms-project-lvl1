e690ff8b0970a06b10d43e4863e25c8cd83119e2
shakhov <shahov.a.a@gmail.com>
2022-05-13 17:35:19 +0300

    Use TF-IDF score for search relevance

--- src/index.js
+++ src/index.js
@@ -1,53 +1,67 @@
 import _ from 'lodash';
 
-const wordRegexp = () => /[\w']+/g;
+const termRegexp = () => /[\w']+/g;
 
-const getTextWords = (text = '') => (
-  text.match(wordRegexp())
+const getTextTerms = (text = '') => (
+  text.match(termRegexp())
 );
 
 const orderByRelevance = (records) => (
-  _.orderBy(records, ['uniqueCount', 'totalCount'], ['desc', 'desc'])
+  _.orderBy(records, ['uniqueCount', 'totalTfIdf'], ['desc', 'desc'])
 );
 
 const buildDocumentIndex = (({ id, text }, initialIndex = {}) => {
-  const documentWords = getTextWords(text);
+  const documentTerms = getTextTerms(text);
+  const termsCount = documentTerms.length;
 
-  return documentWords.reduce((indexAcc, word) => {
-    const wordData = indexAcc[word] || [];
-    const docData = wordData.find((item) => item.id === id);
+  return documentTerms.reduce((indexAcc, term) => {
+    const termData = indexAcc[term] || [];
+    const docData = termData.find((item) => item.id === id);
     const rawCount = (docData?.rawCount || 0) + 1;
-    const newWordData = wordData
-          .filter((item) => item.id !== id)
-          .concat({ id, rawCount });
-    return {...indexAcc, [word]: newWordData };
+    const termFrequency = rawCount / termsCount;
+
+    return {
+      ...indexAcc,
+      [term]: termData
+        .filter((item) => item.id !== id)
+        .concat({ id, rawCount, termFrequency }),
+    };
   }, initialIndex);
 });
 
 const buildSearchEngine = (documents = []) => {
+  const documentsCount = documents.length;
+
   const invertedIndex = documents.reduce((indexAcc, document) => (
     buildDocumentIndex(document, indexAcc)
   ), {});
 
-  console.log(invertedIndex);
+  const inverseDocumentFrequency = (term) => {
+    const docs = invertedIndex[term];
+    return Math.log10(documentsCount / docs.length);
+  };
 
-  const search = (word) => {
-    if (word === '') {
+  const search = (request) => {
+    if (request === '') {
       return [];
     }
 
-    const terms = word.match(wordRegexp());
+    const searchTerms = request.match(termRegexp());
 
-    const searchResult = terms
-      .map((term) => invertedIndex[term] || [])
-      .reduce((resultAcc, termData) => (
-        termData.reduce((termAcc, { id, rawCount }) => {
+    const searchResult = searchTerms
+      .map((term) => [term, invertedIndex[term] || []])
+      .reduce((resultAcc, [term, termData]) => (
+        termData.reduce((termAcc, { id, termFrequency, rawCount }) => {
           const docData = termAcc.find((item) => item.id === id) || [];
           const uniqueCount = (docData.uniqueCount || 0) + 1;
           const totalCount = (docData.totalCount || 0) + rawCount;
+          const tfIdf = termFrequency * inverseDocumentFrequency(term);
+          const totalTfIdf = (docData.totalTfIdf || 0) + tfIdf;
           return termAcc
             .filter((item) => item.id !== id)
-            .concat({ id, uniqueCount, totalCount });
+            .concat({
+              id, uniqueCount, totalCount, totalTfIdf,
+            });
         }, resultAcc)
       ), []);
 
